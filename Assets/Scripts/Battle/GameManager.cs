2cde95520276cd3a1b9a60dd4ea6b0d797fa00cd
NEOGULEE <loes3535@naver.com>
2023-10-09 19:49:36 +0900

    Revert "배틀씬 추가"
    This reverts commit 4d120d39ebc283cee94cb9bcae1e8128257dd395.

---
+++ Assets/Scripts/Battle/GameManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Inst {  get; private set; }
+    void Awake() => Inst = this;
+    [SerializeField] NotificationPanel notificationPanel;
+ 
+    void Start()
+    {
+        StartGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+#if UNITY_EDITOR
+        InputCheatKey();
+#endif
+    }
+
+    void InputCheatKey()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+            TurnManager.OnAddCard?.Invoke(true);
+        if(Input.GetKeyDown(KeyCode.Keypad2))
+            TurnManager.Inst.EndTurn();
+    }
+
+    public void StartGame()
+    {
+        StartCoroutine(TurnManager.Inst.StartGameCo());
+    }
+
+    public void Notification(string message)
+    {
+        notificationPanel.Show(message);
+    }
+}
