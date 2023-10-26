976ecc66f61876f7afbfb91f6481b931f2af78c3
dlgus2383 <dlgus2383@gmail.com>
2023-10-27 04:17:58 +0900

    노드 이펙트 추가

---
+++ Assets/Scripts/Map_Scripts/NodeEffect.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class NodeEffect : MonoBehaviour
+{
+    public float scaleMultiplier = 1.1f;  
+    private Vector3 originalScale;         
+
+    private void Start()
+    {
+        originalScale = transform.localScale;
+    }
+
+    private void OnMouseEnter()
+    {
+        transform.localScale = originalScale * scaleMultiplier;  
+    }
+
+    private void OnMouseExit()
+    {
+        transform.localScale = originalScale;  
+    }
+}
