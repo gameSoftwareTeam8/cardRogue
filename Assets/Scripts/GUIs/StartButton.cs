9fdac0d38fea7b03b15ed2caefcb04a34d7665ad
NEOGULEE <loes3535@naver.com>
2023-11-19 23:40:58 +0900

    Add VFX in main menu

--- Assets/Scripts/GUIs/StartButton.cs
+++ Assets/Scripts/GUIs/StartButton.cs
@@ -5,8 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class StartButton : MonoBehaviour
 {
+    public Canvas canvas;
     public void SceneChange()
     {
-        SceneManager.LoadScene("HeaderBar");
+        StartCoroutine(zoom_in());
+        StartCoroutine(FadeManager.Instance.LoadDiffScene("HeaderBar"));        
+    }
+
+    public IEnumerator zoom_in()
+    {
+        float duration = 1.0f;
+        float current_time = 0.0f;
+        while (current_time < duration)
+        {
+            float current_scale = Mathf.Lerp(1.0f, 1.5f, current_time / duration);
+            canvas.scaleFactor = current_scale;
+            current_time += Time.fixedDeltaTime;
+            yield return new WaitForSeconds(0.01f);
+        }
     }
 }
